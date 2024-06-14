998c5cd6e41cfce3bfa48b7d81c9afa1d31ade47
Juan Amaya <97049133+jamayak@users.noreply.github.com>
2024-06-14 16:02:10 -0500

    Detalle de la orden

--- src/app/pages/orders/order-detail/order-detail.component.ts
+++ src/app/pages/orders/order-detail/order-detail.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { CurrencyPipe, DatePipe, Location } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
-import { AlertsService } from '../../../services/alerts.service';
-import { NgxSpinnerService } from 'ngx-spinner';
-import { OrdersService } from '../../../services/orders.service';
-import { OrderStatuses } from '../../../constants/order-statuses';
+import {Component, OnInit} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {CurrencyPipe, DatePipe, Location} from '@angular/common';
+import {ActivatedRoute} from '@angular/router';
+import {AlertsService} from '../../../services/alerts.service';
+import {NgxSpinnerService} from 'ngx-spinner';
+import {OrdersService} from '../../../services/orders.service';
+import {OrderStatuses} from '../../../constants/order-statuses';
+import {PaymentService} from "../../../services/payment.service";
+import * as mapboxgl from "mapbox-gl";
+import {environment} from "../../../../environments/environment";
 
 @Component({
     selector: 'app-order-detail',
@@ -14,19 +17,28 @@ import { OrderStatuses } from '../../../constants/order-statuses';
 })
 export class OrderDetailComponent implements OnInit {
     public order: any;
+    public payment: any;
 
     public statuses = OrderStatuses;
 
+    public map: mapboxgl.Map;
+    public style = `mapbox://styles/mapbox/streets-v12`;
+    public zoom = 9;
+
     constructor(
         private ordersService: OrdersService,
+        private paymentService: PaymentService,
         private alertsService: AlertsService,
         private spinner: NgxSpinnerService,
         private activatedRoute: ActivatedRoute,
         private location: Location,
-    ) {}
+    ) {
+    }
 
     ngOnInit() {
         this.getOrder();
+
+        this.initMap(-99.133683, 19.438900);
     }
 
     getOrder() {
@@ -36,8 +48,8 @@ export class OrderDetailComponent implements OnInit {
                 const orderUuid = params['uuid'];
                 this.ordersService.getOrder(orderUuid).subscribe({
                     next: (res) => {
-                        this.spinner.hide();
                         this.order = res.order;
+                        this.getPaymentByOrder();
                     },
                     error: (err) => {
                         this.spinner.hide();
@@ -48,7 +60,49 @@ export class OrderDetailComponent implements OnInit {
         });
     }
 
+    getPaymentByOrder() {
+        this.paymentService.getPaymentByOrder(this.order.id).subscribe({
+            next: res => {
+                this.spinner.hide();
+                this.payment = res.payment;
+            },
+            error: err => {
+                this.spinner.hide();
+                this.alertsService.errorAlert(err.error.errors);
+            }
+        })
+    }
+
+    initMap(lng: any, lat: any) {
+        if (this.map) {
+            this.map.remove();
+        }
+
+        this.map = new mapboxgl.Map({
+            accessToken: environment.mapboxToken,
+            container: 'map',
+            style: this.style,
+            zoom: this.zoom,
+            center: [lng, lat]
+        });
+
+        this.map.addControl(new mapboxgl.NavigationControl());
+        this.buildMarker(lng, lat)
+    }
+
+    buildMarker(lng: any, lat: any) {
+        const marker = new mapboxgl.Marker({
+            draggable: true
+        }).setLngLat([lng, lat]).addTo(this.map);
+
+        /* marker.on('dragend', () => {
+             this.longitude.setValue(marker.getLngLat().lng);
+             this.latitude.setValue(marker.getLngLat().lat);
+         });*/
+    }
+
     goBack() {
         this.location.back();
     }
+
 }
