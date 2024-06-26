4432c3236ada6851428969f0ab38c294b0f8ca4c
Juan Amaya <97049133+jamayak@users.noreply.github.com>
2024-06-26 15:27:01 -0500

    Se prepara entorno para pruebas online

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    urlApi: 'http://api.floreriaenvios.com/api',
+    urlApi: 'https://api.floreriaenvios.com/api',
     mapboxToken: 'pk.eyJ1IjoianVhbmFtYXlhayIsImEiOiJjbHZoNG5oZzIxMjlkMmlzMms4b2Q1aHNhIn0.6MnNN5l2rogqVZbgTb03gw',
 };
