513b73129ad4e2c7a90f299447c97d616da54a06
Juan Amaya <amayajuan95@gmail.com>
2024-06-25 20:53:29 -0500

    Se preparan archivos para producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
     production: true,
+    urlApi: 'http://api.floreriaenvios.com/api',
+    mapboxToken: 'pk.eyJ1IjoianVhbmFtYXlhayIsImEiOiJjbHZoNG5oZzIxMjlkMmlzMms4b2Q1aHNhIn0.6MnNN5l2rogqVZbgTb03gw',
 };
