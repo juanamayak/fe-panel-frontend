871905193ffcb4cbd3bb5517fa2338b54e51a919
Juan Amaya <97049133+jamayak@users.noreply.github.com>
2024-07-02 12:10:49 -0500

    Configuración para producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    urlApi: 'https://api.floreriaenvios.com/api',
+    urlApi: 'https://api.floreriaenvios.com:8001/api',
     mapboxToken: 'pk.eyJ1IjoianVhbmFtYXlhayIsImEiOiJjbHZoNG5oZzIxMjlkMmlzMms4b2Q1aHNhIn0.6MnNN5l2rogqVZbgTb03gw',
 };
